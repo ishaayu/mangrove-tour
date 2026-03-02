f2635c21d97e78f4274c1538eab922f0ae094936
ZakatGoldApp <ishaayu03@gmail.com>
2026-03-02 17:16:03 +0800

    Fix Tailwind UI for GitHub Pages

---
+++ docs/out/_next/static/TFkTFnOPZdKXRGNIPPRPP/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mangrove-tour//_next/:path+",
+        "destination": "/mangrove-tour/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/mangrove-tour",
+  assetPrefix: "/mangrove-tour/",
   images: {
     unoptimized: true,
   },
